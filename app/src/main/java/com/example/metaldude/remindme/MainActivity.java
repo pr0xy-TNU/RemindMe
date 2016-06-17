5e831038c1f94ed26b33c4ebd2cb909297b1a963
pr0xy-TNU <yaroslav-tokar@yandex.ru>
2016-06-17 17:19:47 +0300

    Base structure of project

---
+++ app/src/main/java/com/example/metaldude/remindme/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.metaldude.remindme;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ярослав on 17.06.2016.
+ */
+public class MainActivity extends AppCompatActivity
+{
+
+}
